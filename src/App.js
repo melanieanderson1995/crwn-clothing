a72b595b4cfcb362086fdf95c52e8cfd268cdbf2
Melanie Anderson <melaneanderson1995@gmail.com>
2022-02-18 11:59:57 -0500

    Adding shop page and styling

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
+import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
